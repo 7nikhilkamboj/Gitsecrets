c6ee8d5988c0ca31218cd1edae6a94b4407d45de
Nikhil Kamboj <54659447+7nikhilkamboj@users.noreply.github.com>
2025-10-31 10:33:24 +0100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,10 @@
 import os
+from datetime import datetime
 
 api_key = os.getenv("API_KEY")
 #db_password = os.getenv("DB_PASSWORD")
 
 print(f"API Key: {api_key}")
 #print(f"DB Password: {db_password}")
+print("Running scheduled Python job...")
+print("Current UTC time:", datetime.utcnow())
